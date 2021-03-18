261ba9f6ffcfb620e913ccd8e621d946ddf33256
Thomas <thomas.crockett24@gmail.com>
2021-03-18 17:28:31 -0600

    stupid stuff

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       Hello World!
-=======
       Something else is here
->>>>>>> 9a19e40d233ba777afe8ec9292fed6f7fa161590
+      STUPID STUPID STUPID
     </div>
   );
 }
