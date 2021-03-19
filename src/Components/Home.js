af0393cede526b8413801c607d3830b5f93c29d4
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-03-19 09:54:19 -0600

    changes to front end

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            
+            <div>
+                <h1>Recommended For You</h1>
+            </div>
+            <div>
+                <h1>Your Recent Stats</h1>
+            </div>
         </div>
     )
 }
