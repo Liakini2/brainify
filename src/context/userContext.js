dd242c2a062174d0be9baff586dd5eb4f1954165
Jesse Etherington <jesse.etherington@gmail.com>
2021-03-19 13:33:44 -0600

    trying to add changed

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,13 +1,17 @@
-import {createContext, useState} from 'react'
+import React, {createContext, useState} from 'react'
 export const UserContext = createContext()
 
 export const UserProvider = ({children}) => {
-    const [user, setUser] = useState({username: '', firstName: '', lastName: '', email: '', loggedIn: false})
-    return (
-        <UserContext.Provider value={{...user, setUser}}>
+    const [user, setUser] = useState({
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        loggedIn: false
+    })
+    return(
+        <UserContext.Provider value={{user, setUser}}>
             {children}
         </UserContext.Provider>
     )
 }
-
-
