8ffc1ed43766dfd838850032573dd3e3a50f7b62
Thomas <thomas.crockett24@gmail.com>
2021-03-18 18:27:16 -0600

    Added login page, set up some package.json files needed, and added a few endpoints to back end without code in them

---
+++ server/game/gameController.js
@@ -0,0 +1,19 @@
+
+const addScore = (req, res) => {
+
+}
+
+const getScores = (req, res) => {
+
+}
+
+const compareScores = (req, res) => {
+
+}
+
+
+module.exports = {
+    addScore,
+    getScores,
+    compareScores
+}
---
+++ server/user/userController.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcryptjs');
+
+const register = (req, res) => {
+
+}
+
+const login = (req, res) => {
+
+}
+
+const logout = (req, res) => {
+
+}
+
+const getuser = (req, res) => {
+
+}
+
+const updateuser = (req, res) => {
+    
+}
+
+module.exports = {
+    register,
+    login,
+    logout,
+    getuser,
+    updateuser
+}
