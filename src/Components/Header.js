40650bf88650a2aa8c635355b5d39674cb9fa237
Jesse Etherington <jesse.etherington@gmail.com>
2021-03-19 13:16:57 -0600

    added header functionality

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,58 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState, useContext} from 'react'
+import {Link, withRouter} from 'react-router-dom'
+import {UserContext} from '../context/userContext'
+import axios from 'axios'
 
-const Header = () => {
+const Header = (props) => {
+    const [toggle, setToggle] = useState(false)
+    const {username, setUser} = useContext(UserContext)
+
+    const logout = async () => {
+        await axios.post('auth/logout')
+        setUser({username: '', firstName: '', lastName: '', email: '', loggedIn: false})
+        props.history.push('/')
+    }
+
     return (
         <header className='header'>
-            <nav className='nav'>
-                <ul>
-                    <Link to='/home'>
-                        <li>Home</li>
+            <section className='mainBar'>
+
+                <h1>Brainify</h1>
+
+                <div className='headerOptions'>
+                    <nav className='nav'>
+                        <ul>
+                            <Link to='/home'>
+                                <li>Home</li>
+                            </Link>
+                            <Link to='tests'>
+                                <li>Tests</li>
+                            </Link>
+                            <Link to='stats'>
+                                <li>Your Stats</li>
+                            </Link>
+                        </ul> 
+                    </nav>
+
+                    <div className='about'>
+                        <Link to='/about'>
+                            <h3>About</h3>
+                        </Link>
+                        <button onClick={() => setToggle(!toggle)}>{username? username: 'menu'}</button>
+                    </div>
+                </div>
+
+            </section>
+            <section className={`dropDown ${toggle && 'active'}`}>
+
+                    <Link to='/account'>
+                        <h5>Edit Account</h5>
                     </Link>
-                    <Link to='tests'>
-                        <li>Tests</li>
-                    </Link>
-                    <Link to='stats'>
-                        <li>Your Stats</li>
-                    </Link>
-                </ul> 
-            </nav>
-            <div className='about'>
-               <Link to='/about'>
-                    <h3>About</h3>
-               </Link>
-               <button>Placeholder</button>
-            </div>
+                    <button onClick={() => logout()}>Logout</button>
+
+            </section>
         </header>
     )
 }
 
-export default Header
+export default withRouter(Header)
